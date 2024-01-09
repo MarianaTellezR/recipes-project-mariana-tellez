e55de8abb21204505450b0807c058944cd3cfb69
Mariana Téllez <tellezresendizmariana@gmail.com>
2024-01-09 11:18:13 -0600

    Removed header

--- src/App.js
+++ src/App.js
@@ -4,13 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Hello world
-        </p>
-        
-      </header>
+      
     </div>
   );
 }
