98a342327c1719b20a51b1b836ea53637470bad4
Mariana Téllez <tellezresendizmariana@gmail.com>
2024-01-09 11:36:08 -0600

    Updated components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
-import './App.css';
+import "./App.css";
+import Main from "./Main";
 
 function App() {
   return (
     <div className="App">
-      
+      <Main />
+      <h2>Hey</h2>
     </div>
   );
 }
